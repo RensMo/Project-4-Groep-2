3d239a3066869e8c0be1fa456afc68d7ebc85ca9
danismits <danismits@msn.com>
2017-06-21 12:05:03 +0200

    Utils

---
+++ Utils.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIapp
+{
+    class Utils
+    {
+    }
+    interface IinputManager
+    {
+        void OnInput();
+    }
+
+    interface IupdateVisitor
+    {
+        void updateCharacter();
+        void updateProjectile();
+        void updateScreenmanager();
+    }
+    class UpdateVisitor : IupdateVisitor
+    {
+        int Inputmanager;
+
+        public void updateCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateProjectile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateScreenmanager()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface Idrawmanager
+    {
+        void drawRectangle();
+        void drawText();
+        void drawImage();
+    }
+
+    interface IdrawVisitor
+    {
+        void drawCharacter(Character Character);
+        void drawProjectile(Projectile Projectile);
+        void drawScreenManager(Screenmanager ScreenManager);
+    }
+
+    class drawvisitor : IdrawVisitor
+    {
+        int Drawmanager;
+
+        public void drawCharacter(Character Character)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+        public void drawProjectile(Projectile Projectile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawScreenManager(Screenmanager ScreenManager)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
