0c491fafdd36d273b5cb58db0b5e220efe45bdad
Kaana38 <Kaana38@gmail.com>
2017-06-23 12:59:04 +0200

    Database connection windows

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp1
+{
+    public class Example
+    {
+        private static int score;
+
+        static void Main()
+        {
+            string cs = @"server=185.114.157.171;userid=specific_groep2;
+            password=inf1cgroep2;database=specific_project4";
+
+            
+
+            MySqlConnection conn = null;
+            MySqlDataReader rdr = null;
+
+
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+
+                string stm = "SELECT * FROM score";
+                MySqlCommand cmd = new MySqlCommand(stm, conn);
+                rdr = cmd.ExecuteReader();
+                
+
+                while (rdr.Read())
+                {
+
+                    Console.WriteLine(rdr.GetString("Score"));
+
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.ToString());
+
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+
+            }
+            
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+
+
+}
